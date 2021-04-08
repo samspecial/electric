6d6b1093227404734b9167f8fed75b1d20d6e168
Samuel Osinloye <psalmueloye@gmail.com>
2021-04-08 04:08:03 +0100

    Added styles to MenuList

--- src/components/Navigation/MenuList.js
+++ src/components/Navigation/MenuList.js
@@ -1,15 +1,40 @@
 import React from "react";
+import styled from "styled-components";
 
 const MenuList = () => {
     return (
-        <ul>
+        <MenuListGroup>
             <li>Home</li>
             <li>About</li>
             <li>Packages</li>
             <li>Contact</li>
-
-        </ul>
+        </MenuListGroup>
     )
 }
+const MenuListGroup = styled.ul`
+  list-style: none;
+  display: flex;
+  flex-flow: row nowrap;
 
+  li {
+    padding: 18px 10px;
+  }
+
+  @media (max-width: 768px) {
+    flex-flow: column nowrap;
+    background-color: #0D2538;
+    position: fixed;
+    transform: ${({ open }) => open ? 'translateX(0)' : 'translateX(100%)'};
+    top: 0;
+    right: 0;
+    height: 100vh;
+    width: 300px;
+    padding-top: 3.5rem;
+    transition: transform 0.3s ease-in-out;
+
+    li {
+      color: #fff;
+    }
+  }
+`;
 export default MenuList;
