940ba44caa0369b409fd73331dc7572b674d4e60
Samuel Osinloye <psalmueloye@gmail.com>
2021-04-08 03:51:52 +0100

    MenuList component

---
+++ src/components/Navigation/MenuList.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const MenuList = () => {
+    return (
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Packages</li>
+            <li>Contact</li>
+
+        </ul>
+    )
+}
+
+export default MenuList;
