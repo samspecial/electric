b9d550f61b88d32726d53454367b18fd1d667869
Samuel Osinloye <psalmueloye@gmail.com>
2021-05-25 22:12:08 +0100

    ft faq data

---
+++ src/data/faq.js
@@ -0,0 +1,35 @@
+export default [{
+    id: 1,
+    title: "Theft",
+    body: "In situation of theft, we at Electric Rescue would take the full cost oof any damage caused as a result of the unprofessional conduct of our field engineers"
+}, {
+    id: 2,
+    title: "Theft",
+    body: "In situation of theft, we at Electric Rescue would take the full cost oof any damage caused as a result of the unprofessional conduct of our field engineers"
+},
+{
+    id: 3,
+    title: "Theft",
+    body: "In situation of theft, we at Electric Rescue would take the full cost oof any damage caused as a result of the unprofessional conduct of our field engineers"
+
+},
+{
+    id: 4,
+    title: "Theft",
+    body: "In situation of theft, we at Electric Rescue would take the full cost oof any damage caused as a result of the unprofessional conduct of our field engineers"
+},
+{
+    id: 5,
+    title: "Theft",
+    body: "In situation of theft, we at Electric Rescue would take the full cost oof any damage caused as a result of the unprofessional conduct of our field engineers"
+}, {
+    id: 6,
+    title: "Theft",
+    body: "In situation of theft, we at Electric Rescue would take the full cost oof any damage caused as a result of the unprofessional conduct of our field engineers"
+}, {
+    id: 7,
+    title: "Theft",
+    body: "In situation of theft, we at Electric Rescue would take the full cost oof any damage caused as a result of the unprofessional conduct of our field engineers"
+}
+
+]
