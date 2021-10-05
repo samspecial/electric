2f28c1df0423875fdc7dfbad789aeac1f9b8d30c
Samuel Osinloye <psalmueloye@gmail.com>
2021-10-05 21:41:04 +0100

    fixed FAQ Accordion

--- src/components/FAQ/FaqList.jsx
+++ src/components/FAQ/FaqList.jsx
@@ -1,42 +1,20 @@
-import React, { useState } from "react";
-import { SectionHeading } from "../Styles";
-import styled from "styled-components";
-import { FiMinus, FiPlus } from "react-icons/fi";
+import React from "react";
+import { Container, SectionHeading } from "../Styles";
+
 import faqs from "../../data/faq";
 import "../../accordion.css";
+import Accordion from "./Accordion";
 
 const FaqList = () => {
-  const [clicked, setClicked] = useState(null);
-
-  const toggle = (index) => {
-    if (clicked === index) {
-      return setClicked(null);
-    }
-    setClicked(index);
-  };
   return (
-    <Container>
+    <Container background="white">
       <SectionHeading>FAQ</SectionHeading>
       <>
-        {faqs.map((faq, index) => {
+        {faqs.map((faq) => {
           return (
-            <div key={faq.id}>
-              <span className="title" onClick={() => toggle(index)}>
-                {" "}
-                <h4>{faq.title}</h4>{" "}
-                <span>
-                  {clicked === index ? (
-                    <FiMinus color="#d0021b" />
-                  ) : (
-                    <FiPlus color="green" />
-                  )}
-                </span>
-              </span>
-
-              <p className={clicked === index ? "content show" : "content"}>
-                {faq.body}
-              </p>
-            </div>
+            <Accordion key={faq.id} {...faq}>
+              <p className="accordion-text">{faq.body}</p>
+            </Accordion>
           );
         })}
       </>
@@ -45,13 +23,3 @@ const FaqList = () => {
 };
 
 export default FaqList;
-
-const Container = styled.div`
-  width: 40vw;
-  margin: 0 auto;
-
-  @media (max-width: 480px) {
-    width: 100vw;
-    padding: 0 0.8rem;
-  }
-`;
