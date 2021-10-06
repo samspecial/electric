5e754b59d7a511c48b452e0370c390d24be2c366
Samuel Osinloye <psalmueloye@gmail.com>
2021-10-06 23:59:15 +0100

    Added FAQ list

---
+++ src/data/faq.js
@@ -0,0 +1,43 @@
+export default [
+  {
+    id: 1,
+    title: "Discontinue when trial period ends",
+    body:
+      "You are free to discontinue from using the service on/or before the 15days trial period. Failure to do this would cause a deduction to be made on your credit card. Be rest assured that follow up mails woill be sent to notify you of the date your free trial will end.",
+  },
+  {
+    id: 2,
+    title: "Can I upgrade to other plans in future?",
+    body:
+      "Yes you can. The plans are so flexible and tailored to serve the dynamic demands of our client. You can always migrate/upgrade to any plan that suits your household need.",
+  },
+  {
+    id: 3,
+    title: "Can I rollover",
+    body: "You can not rollover any unclaimed benefits to the next month.",
+  },
+  {
+    id: 4,
+    title: "Damage exceeds monthly subscription",
+    body:
+      "So long as the customer pay his/her monthly premium on or before due date, we will not hold back to settle every of his/her household electrical fault bill.",
+  },
+  {
+    id: 5,
+    title: "What becomes my fate if i default",
+    body:
+      "Defaulting to remit your premium for the month is going through the month without cover and we would not be held liable to act if such household experience power problem during this time.",
+  },
+  {
+    id: 6,
+    title: "Theft",
+    body:
+      "In situation of theft, we at Electric Rescue would take the full cost oof any damage caused as a result of the unprofessional conduct of our field engineers",
+  },
+  {
+    id: 7,
+    title: "Theft",
+    body:
+      "In situation of theft, we at Electric Rescue would take the full cost oof any damage caused as a result of the unprofessional conduct of our field engineers",
+  },
+];
