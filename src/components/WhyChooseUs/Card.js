06c734bde813e64b6cc3d0a5796db37f9f6e9d10
Samuel Osinloye <psalmueloye@gmail.com>
2022-01-14 23:32:35 +0100

    fixed why-choose-us icons

--- src/components/WhyChooseUs/Card.js
+++ src/components/WhyChooseUs/Card.js
@@ -1,12 +1,16 @@
 import React from "react";
-import { Title, PanelHorizontal, TextContainer, Img } from "./CardStyle";
+import { Title, PanelHorizontal, TextContainer, Wrapper } from "./CardStyle";
 import PropTypes from "prop-types";
 import { Text } from "../Styles";
 
 const Card = ({ title, icon, text }) => {
+  let Icon = icon;
   return (
     <PanelHorizontal id={title}>
-      <Img src={icon} alt="" />
+      <Wrapper>
+        <Icon style={{ fontSize: "13px", marginTop: "8px" }}></Icon>
+      </Wrapper>
+
       <TextContainer>
         <Title>{title}</Title>
         <Text style={{ width: "auto", fontSize: "0.9rem", color: "black" }}>
@@ -19,7 +23,7 @@ const Card = ({ title, icon, text }) => {
 
 Card.propTypes = {
   title: PropTypes.string.isRequired,
-  icon: PropTypes.string.isRequired,
+  icon: PropTypes.func.isRequired,
   text: PropTypes.string.isRequired,
 };
 export default Card;
